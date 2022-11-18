8e5f79ba86c82246ad869163ede660f2e28fd9cf
tri17 <trilestari11201@gmail.com>
2022-11-18 23:24:07 +0700

    zetta learning angular day11

--- angular_master/angular_learning/src/app/app.component.ts
+++ angular_master/angular_learning/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { DataService } from './service/data.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_learning';
+  title = 'Angular Day 11';
 }
